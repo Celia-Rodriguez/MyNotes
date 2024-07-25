9ddc4aec11b64126b24c49c6cb3397c972d8329b
Celia-Rodriguez <120595927+Celia-Rodriguez@users.noreply.github.com>
2024-07-25 13:14:29 +0200

    changes styles

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+Crear, editar y borrar tarjetas de tareas
+poder ver el tiempo de hoy en la eregión del ordenador??
+clasificar las tareas por hacer en ello y hechas
+al situar el raton sobre la tarejta de la carta que aprecan las opciones de edicion y que se pueda interactuar con ellas (ref google keeps)
+al pinchar sobre la tarjeta se abre un  poopup con la taerjeta mas grande y las opciones d dicion
+anclar notas o recordatorio al inicio?
+estilo de las notas editables?
+dos tipos de tarjetas notas y recordartorios esto tienen una hora y fecha editable.
+
+*/
